9992a7bdf4062a8475cf0051975a80839a5be77e
Sam Hobson <shhobson@protonmail.com>
2023-11-17 21:03:09 +1100

    Updated execution script

--- run-ansible-playbook.sh
+++ run-ansible-playbook.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if command -v "ansible-playbook" &> /dev/null; then
+  if command -v "apt" &> /dev/null; then
+    ./ansible-install-apt.sh
+  fi
+fi
+
 # Ask the user for a password
-read -r -s -p "Enter your password: " USER_PASSWORD
-echo
+# read -r -s -p "Enter your password: " USER_PASSWORD
+# echo
 
 ansible-playbook ./local.yml -K -e "user_pass=$USER_PASSWORD"
